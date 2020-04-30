bce17461d9a41b7f3d0a8fced51d264bbe4f5faa
dobrynin13 <64210455+dobrynin13@users.noreply.github.com>
2020-04-30 11:52:33 +0300

    Add files via upload
    Лабораторные работы по классам 1-2

---
+++ Лаба 1-2к/Cost()/Price/Price.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+using namespace std;
+struct fraction
+{
+	int n;
+	int m;
+	int first;
+	int second;
+	void Init(int, int);
+	void Read();
+	void Show();
+	int Cost(int m);
+};
+void fraction::Init(int F, int S)
+{
+	first = F;
+	second = S;
+}
+
+void fraction::Read()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Цена товара\n";
+	cin >> first;
+	cout << "Количество единиц данного товара\n";
+	cin >> second;
+}
+void fraction::Show()
+{
+	cout << "Цена товара == " << first << endl;
+	cout << "Количество единиц данного товара == " << second << endl;
+}
+int fraction::Cost(int m)
+{
+	return (first * second);
+}
+fraction make_fraction(int F, int S)
+{
+	fraction t;
+	t.Init(F, S);
+	return t;
+}
+
+void main(int n,int m)
+{
+	
+	setlocale(LC_ALL, "rus");
+	fraction A;
+	fraction B;
+	fraction* X = new fraction;
+
+	cout << "Объект А\n";
+	A.Init(13013, 18);
+	A.Show();
+
+	cout << "\nОбъект B\n";
+	B.Read();
+	B.Show();
+
+	cout << "\nОбъект X\n";
+	X->Init(1478, 13);
+	X->Show();
+
+	cout << "\nA.Cost(" << A.first << "\, " << A.second << ") = " << A.Cost(m) << "\n";
+	cout << "B.Cost(" << B.first << "\, " << B.second << ") = " << B.Cost(m) << "\n";
+	cout << "X.Cost(" << X->first << "\, " << X->second << ") = " << X->Cost(m) << "\n";
+
+	cout << "\nМассив объектов";
+	cout << "\nВведите размер массива: ";
+	cin >> n;
+
+	fraction* mass = new fraction[n];
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Объект №" << i + 1 << ":\n";
+		mass[i].Read();
+	}
+	cout << "\n\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Объект №" << i + 1 << ":\n";
+		mass[i].Show();
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << "mass[" << i << "].Cost(" << mass[i].first << ", " << mass[i].second << ") = " << mass[i].Cost(m) << "\n";
+	}
+}
+
---
+++ Лаба 1-2к/Route()/Route/Route.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Route
+{
+	string otpr;
+	string naz;
+	float time;
+
+
+public:
+
+	Route();
+	Route(string, string, float);
+	Route(const Route&);
+	~Route();
+	string get_otpr();
+	void set_otpr(string);
+	string get_naz();
+	void set_naz(string);
+	float get_time();
+	void set_time(float);
+	void show();
+};
+Route::Route()
+{
+	setlocale(LC_ALL, "rus");
+	otpr = "";
+	naz = "";
+	time = 0;
+	cout << "Конструктор без параметров для объекта" << this << endl;
+}
+
+Route::Route(string Ot, string N, float T)
+{
+	setlocale(LC_ALL, "rus");
+	otpr = Ot;
+	naz = N;
+	time = T;
+	cout << "Конструктор c параметрами для объекта" << this << endl;
+}
+
+Route::Route(const Route& t)
+{
+	setlocale(LC_ALL, "rus");
+	otpr = t.otpr;
+	naz = t.naz;
+	time = t.time;
+	cout << "Конструктор копирования для объекта" << this << endl;
+}
+
+Route::~Route()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Деструктор для объекта" << this << endl;
+}
+
+//селекторы
+string Route::get_otpr()
+{
+	return otpr;
+}
+
+string Route::get_naz()
+{
+	return naz;
+}
+
+float Route::get_time()
+{
+	return time;
+}
+
+//Модификаторы
+
+void Route::set_otpr(string Ot)
+{
+	otpr = Ot;
+}
+
+void Route::set_naz(string N)
+{
+	naz = N;
+}
+
+void Route::set_time(float T)
+{
+	time = T;
+}
+
+void Route::show()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Пункт отправления ->" << otpr << endl;
+	cout << "Пункт назначения ->" << naz << endl;
+	cout << "Время в пути ->" << time << endl;
+}
+Route make_route()
+{
+	setlocale(LC_ALL, "rus");
+	string ot;
+	string n;
+	float t;
+
+	cout << "Введите пункт отправления\n";
+	getline(cin, ot);
+	cout << "Введите пункт назначения\n";
+	getline(cin, n);
+	cout << "Введите время в пути\n";
+	cin >> t;
+	Route time(ot, n, t);
+	return time;
+}
+
+void print_route(Route t)
+{
+	t.show();
+}
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	Route c1;
+	c1.show();
+	Route c2("Perm", "Solikamsk", 3.13);
+	c2.show();
+	Route c3 = c2;
+	c3.set_otpr("Moscow");
+	c3.set_naz("Kazan");
+	c3.set_time(6.59);
+
+	print_route(c3);
+
+	c1 = make_route();
+	c1.show();
+}
